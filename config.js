f0412f37d667333597e634d5c35e8f0a6ba51599
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2019-01-19 15:59:39 +0100

    a

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const ENDPOINT_DEV = `http://localhost:5656`;
+export const ENDPOINT_DEV = `http://localhost:5555`;
 // export const ENDPOINT_DEV = `https://inspinder-yoga-prod.herokuapp.com/`;
 export const ENDPOINT_PROD = `https://inspinder-yoga-prod.herokuapp.com/`;
