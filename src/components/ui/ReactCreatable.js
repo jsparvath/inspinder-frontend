12027281a35cd6efdbc5672eba824857032966a5
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2019-05-02 10:22:37 +0200

    updated to next 8

--- src/components/ui/ReactCreatable.js
+++ src/components/ui/ReactCreatable.js
@@ -1,214 +1,233 @@
 /* eslint-disable react/prop-types, react/jsx-handler-names */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import CreatableSelect from 'react-select/lib/Creatable';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import NoSsr from '@material-ui/core/NoSsr';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Chip from '@material-ui/core/Chip';
-import MenuItem from '@material-ui/core/MenuItem';
-import CancelIcon from '@material-ui/icons/Cancel';
-import { emphasize } from '@material-ui/core/styles/colorManipulator';
+import Chip from "@material-ui/core/Chip";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import { emphasize } from "@material-ui/core/styles/colorManipulator";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import CancelIcon from "@material-ui/icons/Cancel";
+import classNames from "classnames";
+import React from "react";
+import CreatableSelect from "react-select/lib/Creatable";
 
-const styles = (theme) => ({
-	root: {
-		flexGrow: 1,
-		// height:
-	},
-	input: {
-		display: 'flex',
-		padding: 0
-	},
-	valueContainer: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		flex: 1,
-		alignItems: 'center',
-		overflow: 'hidden'
-	},
-	chip: {
-		margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
-	},
-	chipFocused: {
-		backgroundColor: emphasize(
-			theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
-			0.08
-		)
-	},
-	noOptionsMessage: {
-		padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
-	},
-	singleValue: {
-		fontSize: 16
-	},
-	placeholder: {
-		position: 'absolute',
-		left: 2,
-		fontSize: 16
-	},
-	paper: {
-		position: 'absolute',
-		zIndex: 1,
-		marginTop: theme.spacing.unit,
-		left: 0,
-		right: 0
-	},
-	divider: {
-		height: theme.spacing.unit * 2
-	}
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+    // height:
+  },
+  input: {
+    display: "flex",
+    padding: 0
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: "absolute",
+    left: 2,
+    fontSize: 16
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0
+  },
+  divider: {
+    height: theme.spacing.unit * 2
+  }
 });
 
 function NoOptionsMessage(props) {
-	return (
-		<Typography color="textSecondary" className={props.selectProps.classes.noOptionsMessage} {...props.innerProps}>
-			<span>{props.children}</span>
-		</Typography>
-	);
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      <span>{props.children}</span>
+    </Typography>
+  );
 }
 
 function inputComponent({ inputRef, ...props }) {
-	return <div ref={inputRef} {...props} />;
+  return <div ref={inputRef} {...props} />;
 }
 
 function Control(props) {
-	return (
-		<TextField
-			fullWidth
-			// label="yo"
-			InputProps={{
-				inputComponent,
-				inputProps: {
-					className: props.selectProps.classes.input,
-					inputRef: props.innerRef,
-					children: props.children,
-					...props.innerProps
-				}
-			}}
-			{...props.selectProps.textFieldProps}
-		/>
-	);
+  return (
+    <TextField
+      fullWidth
+      // label="yo"
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: props.selectProps.classes.input,
+          inputRef: props.innerRef,
+          children: props.children,
+          ...props.innerProps
+        }
+      }}
+      {...props.selectProps.textFieldProps}
+    />
+  );
 }
 
 function Option(props) {
-	return (
-		<MenuItem
-			buttonRef={props.innerRef}
-			selected={props.isFocused}
-			component="div"
-			style={{
-				fontWeight: props.isSelected ? 500 : 400
-			}}
-			{...props.innerProps}
-		>
-			{props.children}
-		</MenuItem>
-	);
+  return (
+    <MenuItem
+      buttonRef={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
 }
 
 function Placeholder(props) {
-	return (
-		<Typography color="textSecondary" className={props.selectProps.classes.placeholder} {...props.innerProps}>
-			<span>{props.children()}</span>
-		</Typography>
-	);
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.placeholder}
+      {...props.innerProps}
+    >
+      <span>{props.children()}</span>
+    </Typography>
+  );
 }
 
 function SingleValue(props) {
-	return (
-		<Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
-			<span>{props.children}</span>
-		</Typography>
-	);
+  return (
+    <Typography
+      className={props.selectProps.classes.singleValue}
+      {...props.innerProps}
+    >
+      <span>{props.children}</span>
+    </Typography>
+  );
 }
 
 function ValueContainer(props) {
-	return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+  return (
+    <div className={props.selectProps.classes.valueContainer}>
+      {props.children}
+    </div>
+  );
 }
 
 function MultiValue(props) {
-	return (
-		<Chip
-			tabIndex={-1}
-			label={props.children}
-			className={classNames(props.selectProps.classes.chip, {
-				[props.selectProps.classes.chipFocused]: props.isFocused
-			})}
-			onDelete={props.removeProps.onClick}
-			deleteIcon={<CancelIcon {...props.removeProps} />}
-		/>
-	);
+  return (
+    <Chip
+      tabIndex={-1}
+      label={props.children}
+      className={classNames(props.selectProps.classes.chip, {
+        [props.selectProps.classes.chipFocused]: props.isFocused
+      })}
+      onDelete={props.removeProps.onClick}
+      deleteIcon={<CancelIcon {...props.removeProps} />}
+    />
+  );
 }
 
 function Menu(props) {
-	return (
-		<Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
-			{props.children}
-		</Paper>
-	);
+  return (
+    <Paper
+      square
+      className={props.selectProps.classes.paper}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Paper>
+  );
 }
 
 const components = {
-	Control,
-	Menu,
-	MultiValue,
-	NoOptionsMessage,
-	Option,
-	Placeholder,
-	SingleValue,
-	ValueContainer,
-	ClearIndicator: () => null
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+  ClearIndicator: () => null
 };
 
 class ReactCreatable extends React.Component {
-	state = {
-		single: null,
-		multi: null
-	};
+  state = {
+    single: null,
+    multi: null
+  };
 
-	handleChange = (name) => (value) => {
-		this.setState({
-			[name]: value
-		});
-	};
+  handleChange = name => value => {
+    this.setState({
+      [name]: value
+    });
+  };
 
-	render() {
-		const { classes, theme } = this.props;
+  render() {
+    const { classes, theme } = this.props;
 
-		const selectStyles = {
-			input: (base) => ({
-				...base,
-				color: theme.palette.text.primary,
-				'& input': {
-					font: 'inherit'
-				}
-			})
-		};
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        "& input": {
+          font: "inherit"
+        }
+      })
+    };
 
-		return (
-			<div className={classes.root}>
-				{/* <NoSsr> */}
-				<CreatableSelect
-					classes={classes}
-					styles={selectStyles}
-					textFieldProps={{
-						label: 'tags'
-					}}
-					components={components}
-					placeholder={() => undefined}
-					value={this.props.value}
-					onChange={this.props.onChange}
-					options={this.props.options}
-					isMulti={this.props.isMulti}
-				/>
-				{/* </NoSsr> */}
-			</div>
-		);
-	}
+    return (
+      <div className={classes.root}>
+        {/* <NoSsr> */}
+        <CreatableSelect
+          classes={classes}
+          styles={selectStyles}
+          textFieldProps={{
+            label: "tags"
+          }}
+          components={components}
+          placeholder={() => undefined}
+          value={this.props.value}
+          onChange={this.props.onChange}
+          options={this.props.options}
+          isMulti={this.props.isMulti}
+        />
+        {/* </NoSsr> */}
+      </div>
+    );
+  }
 }
 
 const RC = withStyles(styles, { withTheme: true })(ReactCreatable);
-export { RS as ReactSelect, RC as ReactCreatable };
+export { RC as ReactCreatable };
